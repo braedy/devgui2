3bf8ef12708b9546e358f12a929e52d8b547fca3
Agulfy <agulfy@users.noreply.github.com>
2014-06-04 22:30:35 +0300

    external drag and drop for custom background images

--- Scripts/form.js
+++ Scripts/form.js
@@ -151,3 +151,36 @@ $(document).foundation({
     }
   }
 });
+
+function dropExternal(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  var dt = e.dataTransfer;
+  var files = dt.files;
+
+  handleFiles(files);
+  return false;
+}
+
+function handleFiles(files) {
+  for (var i = 0; i < files.length; i++) {
+    var file = files[i];
+    var imageType = /image.*/;
+
+    if (!file.type.match(imageType)) {
+      continue;
+    }
+
+    var gameCanvas = document.getElementById("gameCanvas").childNodes[0];
+
+    var img = document.createElement("img");
+    img.src = window.URL.createObjectURL(files[i]);
+    img.height = 60;
+    img.onload = function(e) {
+    window.URL.revokeObjectURL(this.src);
+    }
+
+    gameCanvas.style.backgroundImage="url("+img.src+")";
+  }
+}
